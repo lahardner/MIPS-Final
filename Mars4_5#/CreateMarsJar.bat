f118d1e163c4be6921a76dae89f91e966531ddc4
jahardner <rocksrollsrules@gmail.com>
2017-01-01 16:50:28 -0500

    Add files via upload

---
+++ Mars4_5#/CreateMarsJar.bat
@@ -0,0 +1,1 @@
+"\Program Files\Java\jdk1.8.0_101\bin\jar" cmf mainclass.txt Mars.jar PseudoOps.txt Config.properties Syscall.properties Settings.properties MARSlicense.txt mainclass.txt MipsXRayOpcode.xml registerDatapath.xml controlDatapath.xml ALUcontrolDatapath.xml CreateMarsJar.bat Mars.java Mars.class docs help images mars 
